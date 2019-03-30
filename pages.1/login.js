af309cefe94a9ccb78061f33537f261cc16c1ab7
anhtu <anhtudo97@gmail.com>
2019-03-30 17:45:44 +0700

    rm  page 1

--- pages.1/login.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Link from 'next/link'
-import Login from '../components/Login'
-
-const login = () => <Login />
-
-export default login
--- pages.1/signup.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Link from 'next/link'
-import Signup from '../components/Signup'
-
-const signup = () => <Signup />
-
-export default signup
